eaa4e8ae37d980216a022c01d92e6d8e8f5d6ef6
unknown <chengjs2018@gmail.com>
2022-09-15 23:54:52 +0700

    Node CRUD

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
 "# node-baise-api1" 
 
-#### Start create nodejs project####
-1. npm init -y (Create nodejs project)
-2. npm i express mysql (install package)
+<!-- #### Start create nodejs project#### -->
+"1. npm init -y (Create nodejs project)"
+2. npm i cors express mysql (install package)
+
+# Incase Connect Mysql error as: Error: ER_NOT_SUPPORTED_AUTH_MODE: Client does not support authentication protocol requested by server; consider upgrading MySQL client
+- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+- flush privileges;
+
+
+<!-- ##################Allows Port (CORS)################### -->
+#####Allows port only client access server#####
+const cors = require('cors');
+app.use(cors({
+    origin: 'https://www.section.io'
+}));
+
+<!-- #####Allows 2 clients access to server#### -->
+const cors = require('cors');
+app.use(cors({
+    origin: ['https://www.section.io', 'https://www.google.com/']
+}));
+
+<!-- ######Allows N or more client and not limit access to server#### -->
+app.use(cors({
+    origin: '*'
+}));
+<!-- ##################end Allows Port################### -->
+<!-- ################allows header access######## -->
+app.use(cors({
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
+See detail at: https://www.section.io/engineering-education/how-to-use-cors-in-nodejs-with-express/
+<!-- ################end allows header access######## -->
+
+
 
--- server.js
+++ server.js
@@ -1,6 +1,123 @@
-const express=require('express');
-const mysql=require('mysql');
+const express = require("express");
+const mysql = require("mysql");
+const cors=require('cors')
 
-const app=express();
+const app = express();
+app.use(cors({
+    origin:['*']
+    //origin:'http://localhost:3000'
+}));//allows  port
+app.use(express.json());
 
-app.listen(5000,()=>console.log('Server is running on port 5000'));
+// MySQL Connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "chengsoft",
+  database: "node_apidb1",
+  port:'3306'
+});
+
+connection.connect((err)=>{
+    if(err){
+        console.log('Error connecting to MySQL database = ',err)
+        return
+    }
+    console.log('MySQL successfully connect.')
+})
+
+//Create Routes
+app.post("/user",async(req,res)=>{
+    const {name,email,password}=req.body;
+    try{
+        connection.query("INSERT INTO users(fullname,email,password)VALUES(?,?,?)",
+        [name,email,password],
+        (err,results,fields)=>{
+            if(err){
+                console.log(err)
+                return res.status(400).send();
+            }
+            return res.status(201).json({message:"Created user successfully."})
+        })
+    }catch(err){
+        console.log(err)
+        return res.status(500).send();
+    }
+})
+
+//Read
+app.get("/user",async(req,res)=>{
+    try{
+        connection.query("SELECT * FROM users",(err,results,fields)=>{
+            if(err){
+                console.log(err);
+                return res.status(400).send();
+            }
+            res.status(200).json(results);
+        })
+    }catch(err){
+        console.log(err)
+        return res.status(500).send()
+    }
+})
+
+//search
+app.get("/user/search/:email",async(req,res)=>{
+    const email=req.params.email;
+
+    try{
+        connection.query("SELECT * FROM users WHERE email=?",
+        [email],(err,results,fields)=>{
+            if(err){
+                console.log(err)
+                return res.status(400).send()
+            }
+            res.status(200).json(results)
+        })
+    }catch(err){
+        console.log(err)
+        return res.status(500).send();
+    }
+})
+
+//Update
+app.patch("/user/:email",async(req,res)=>{
+    const email=req.params.email;
+    const newPassword=req.body.password;
+
+    try {
+        connection.query("UPDATE users SET password=? WHERE email=?",[newPassword,email],(err,results,fields)=>{
+            if(err){
+                console.log(err)
+                return res.status(400).send()
+            }
+            console.log(results)
+            res.status(200).json({message:"Update user successfully."})
+        })
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send();
+    }
+})
+
+//Delete
+app.delete("/user/:id",async(req,res)=>{
+    const userId=req.params.id;
+    try {
+        connection.query("DELETE FROM users WHERE id=?",[userId],(err,results,fields)=>{
+            if(err){
+                console.log(err)
+                return res.status(400).send()
+            }
+            if(results.affectedRows===0){
+                return res.status(404).json({message:"Not found user id"})
+            }
+            res.status(200).json({message:"Delete user succesfully"})
+        })
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send()
+    }
+})
+
+app.listen(5000, () => console.log("Server is running on port 5000"));
